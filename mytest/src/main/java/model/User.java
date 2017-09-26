e8ed0f9d3e988c8f3e9dd9c24fa041ea1ca942c7
ruyanfly <yanru545@126.com>
2017-09-26 17:38:18 +0800

    添加hibernate

--- mytest/src/main/java/model/User.java
+++ mytest/src/main/java/model/User.java
@@ -7,12 +7,13 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity//声明当前类为hibernate映射到数据库中的实体类
-@Table(name = "ruyanfly_user")//声明在数据库中自动生成的表名为t_user
+@Table(name = "ruyanfly_user")//声明在数据库中自动生成的表名为ruyanfly_user
 public class User {
     @Id//声明此列为主键
     
     //根据不同数据库自动选择合适的id生成方案,这里使用mysql,为递增型
-    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     
     private Integer id;
     private String name;
--- mytest/src/test/java/hibernateTest/UserTest.java
+++ mytest/src/test/java/hibernateTest/UserTest.java
@@ -11,27 +11,29 @@ import model.User;
 
 //@Test//我们使用junit进行测试
 public class UserTest {
-	/***
-	 * 相对于3.x.x版本hibernate，我们在4.x.x采用如下方式获取我们的会话工厂：
-	 * 1.解析我们在hibernate.cfg.xml中的配置
-	 * Configuration configuration = new Configuration().configure();
-	 * 2.创建服务注册类,进一步注册初始化我们配置文件中的属性
-	 * ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
-	 * 3.创建我们的数据库访问会话工厂
-	 * SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-	 * 但在5.1.0版本汇总，hibernate则采用如下新方式获取：
-	 * 1. 配置类型安全的准服务注册类，这是当前应用的单例对象，不作修改，所以声明为final
-	 * 在configure("cfg/hibernate.cfg.xml")方法中，如果不指定资源路径，默认在类路径下寻找名为hibernate.cfg.xml的文件
-	 * 2. 根据服务注册类创建一个元数据资源集，同时构建元数据并生成应用一般唯一的的session工厂
-	 */
-    final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("cfg/hibernate.cfg.xml").build();
-    SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-
-    /****上面是配置准备，下面开始我们的数据库操作******/
-    Session session = sessionFactory.openSession();//从会话工厂获取一个session
-    Transaction transaction = session.beginTransaction();//开启一个新的事务
-    User user = new User();
-    user.setName("ruyanfly");
-    session.save(user);
-    transaction.commit();//提交事务
+	public static void main(String[] args){
+		/***
+		 * 相对于3.x.x版本hibernate，我们在4.x.x采用如下方式获取我们的会话工厂：
+	     * 1.解析我们在hibernate.cfg.xml中的配置
+	     * Configuration configuration = new Configuration().configure();
+	     * 2.创建服务注册类,进一步注册初始化我们配置文件中的属性
+	     * ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+	     * 3.创建我们的数据库访问会话工厂
+	     * SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+	     * 但在5.1.0版本汇总，hibernate则采用如下新方式获取：
+	     * 1. 配置类型安全的准服务注册类，这是当前应用的单例对象，不作修改，所以声明为final
+	     * 在configure("hibernate.cfg.xml")方法中，如果不指定资源路径，默认在类路径下寻找名为hibernate.cfg.xml的文件
+	     * 2. 根据服务注册类创建一个元数据资源集，同时构建元数据并生成应用一般唯一的的session工厂
+	     */
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+		SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+		
+		/****上面是配置准备，下面开始我们的数据库操作******/
+		Session session = sessionFactory.openSession();//从会话工厂获取一个session
+		Transaction transaction = session.beginTransaction();//开启一个新的事务
+		User user = new User();
+		user.setName("ruyanfly");
+		session.save(user);
+		transaction.commit();//提交事务
+	}
 }
